f191f8a63a612b72f93ed4425d27dd2c16b8fb20
Kartik Gupta <kartikgupta1474@gmail.com>
2026-07-21 05:46:09 +0530

    add design tokens and global styles

--- apps/viewer/src/main.tsx
+++ apps/viewer/src/main.tsx
@@ -3,6 +3,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./App";
+import "./styles/tokens.css";
+import "./styles/global.css";
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { staleTime: 30_000, refetchOnWindowFocus: false } },
